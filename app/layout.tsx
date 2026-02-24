b172573ae637307d9d916572fdfa9ca6ba3fe45b
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2026-02-24 14:20:59 -0600

    added script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6664558533985701"
+      crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
